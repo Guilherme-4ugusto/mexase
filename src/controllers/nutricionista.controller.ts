1f2d27e6eb61e91cb11f6fffee6c81e0ae3fd744
Guilherme Augusto <guilhermeabscontato@gmail.com>
2025-10-16 09:38:03 -0300

    adição de novos metodos relacionados ao nutricioniesta (#11)

--- src/controllers/nutricionista.controller.ts
+++ src/controllers/nutricionista.controller.ts
@@ -49,23 +49,39 @@ export const reativarNutricionista = async (req: Request, res: Response) => {
 }
 
 export const atualizarDadosNutricionista = async (req: Request, res: Response) => {
+  const { id } = req.params;
   const dto = plainToInstance(CriarNutricionistaDTO, req.body);
 
-  const erros = await validate(dto);
+  const erros = await validate(dto, { skipMissingProperties: true });
 
   if (erros.length > 0) {
     const mensagens = erros.map(e => Object.values(e.constraints || {})).flat();
-
     return res.status(400).json({ errors: mensagens });
   }
 
   try {
-    const novoNutricionista = await service.criar(dto);
-    return res.status(201).json(novoNutricionista);
+    const nutricionistaAtualizado = await service.atualizar(parseInt(id), dto);
+    return res.status(200).json(nutricionistaAtualizado);
   } catch (error: any) {
-    if (error.message === 'Email ou matricula já cadastrado') {
+    if (error.message === 'Nutricionista não encontrado') {
+      return res.status(404).json({ error: error.message });
+    }
+    if (error.message === 'Email ou matrícula já cadastrados por outro nutricionista') {
       return res.status(409).json({ error: error.message });
     }
     return res.status(500).json({ error: 'Erro interno do servidor' });
   }
-};
+}
+
+export const buscarNutricionista = async (req: Request, res: Response) => {
+  try{
+    const { id } = req.params
+    const nutricionistas = await service.buscar(parseInt(id));
+    return res.status(200).json(nutricionistas);
+  } catch (error: any) {
+    if(error.message == 'Nenhum nutricionista encontrado'){
+      return res.status(404).json({ error: error.message });
+    }
+    return res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+}
